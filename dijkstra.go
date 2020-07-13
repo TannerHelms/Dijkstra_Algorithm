bc30ddf476e921340bf1d08584750fcf21432720
TannerHelms <tannerhelms01@gmail.com>
2020-07-12 22:08:30 -0600

    Add files via upload

--- dijkstra.go
+++ dijkstra.go
@@ -20,11 +20,65 @@ type Node struct {
 	Name string
 }
 
+// Function that implemts the Dijkstra algorithm
+// Returns the shortes path form the startNode to all other Nodes
+func (g *Graph) Dijkstra(startNode *Node) (shortestDistanceTable string) {
+
+	// Create a Distance table with all distances as Infinity except for startNode which is zero
+
+	distanceTable := g.NewDistanceTable(startNode)
+
+	// Create an array that will hold the visited Nodes
+
+	var visited []*Node
+
+	//Start a loop to visit all the Nodes
+
+	for len(visited) != len(g.Nodes) {
+
+		//Get the closest Node that hasnt been visited with the least amount of distance
+
+		node := GetClosestNonVisitedNode(distanceTable, visited)
+
+		// Mark the node as visited
+
+		visited = append(visited, node)
+
+		// Get the Nodes neighbors
+
+		nodeNeighbors := g.GetNodeEdges(node)
+
+		for _, neighbor := range nodeNeighbors {
+
+			//The distance to that neighbor
+
+			distanceToNeighbor := distanceTable[node] + neighbor.Distance
+
+			//If the distance is less then what is currently in the costTable then update it
+
+			if distanceToNeighbor < distanceTable[neighbor.Child] {
+				distanceTable[neighbor.Child] = distanceToNeighbor
+			}
+		}
+	}
+
+	// Remake the distanceTable
+
+	for node,distance := range distanceTable {
+		shortestDistanceTable += fmt.Sprintf("Distance from %s to %s = %d\n", startNode.Name, node.Name, distance)
+	}
+
+	return shortestDistanceTable
+
+}
+
+
+
 const Infinity = int(^uint(0) >> 1)
 
 // Insert the Nodes into the Graph
 
-func (g *Graph) insertNodes(node ...*Node) {
+func (g *Graph) InsertNodes(node ...*Node) {
 	for _,v := range node {
 		g.Nodes = append(g.Nodes, v)
 	}
@@ -32,7 +86,7 @@ func (g *Graph) insertNodes(node ...*Node) {
 
 //Insert the edges between Node's into the Graph
 
-func (g *Graph) insertEdges(edge ...*Edge) {
+func (g *Graph) InsertEdges(edge ...*Edge) {
 	for _,v := range edge {
 		g.Edges = append(g.Edges, v)
 	}
@@ -40,7 +94,7 @@ func (g *Graph) insertEdges(edge ...*Edge) {
 
 // Function that Prints the edges to the console
 
-func (g *Graph) toString() {
+func (g *Graph) ToString() {
 	for _,v := range g.Edges {
 		fmt.Printf("%v %v %v\n", *v.Parent, v.Distance, *v.Child)
 	}
@@ -71,7 +125,7 @@ func (g *Graph) NewDistanceTable(startNode *Node) map[*Node]int {
 
 // This function returns a slice of all the Nodes neighbors
 
-func (g *Graph) getNodeEdges(node *Node) (edges []*Edge) {
+func (g *Graph) GetNodeEdges(node *Node) (edges []*Edge) {
 
 	// Sort through all the edges in Graph
 
@@ -89,7 +143,7 @@ func (g *Graph) getNodeEdges(node *Node) (edges []*Edge) {
 // Function that returns the closest Node with the lease amount of distance
 // If the Node hasn't been visited yet
 
-func getClosestNonVisitedNode (costTable map[*Node]int, visited []*Node) *Node {
+func GetClosestNonVisitedNode (costTable map[*Node]int, visited []*Node) *Node {
 	type CostTableToSort struct {
 		Node     *Node
 		Distance int
